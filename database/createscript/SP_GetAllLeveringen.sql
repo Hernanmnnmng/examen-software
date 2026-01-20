5459d837428f245b85284884fa0232f09848255e
S B <sb@MacBook-Air-van-S.local>
2026-01-20 09:29:41 +0100

    SP_GetAllLeveringen done

--- database/createscript/SP_GetAllLeveringen.sql
+++ database/createscript/SP_GetAllLeveringen.sql
@@ -1,11 +1,17 @@
-DROP PROCEDURE IF EXISTS SP_GetAllLeveringen
+DROP PROCEDURE IF EXISTS SP_GetAllLeveringen;
 DELIMITER ??
 
 CREATE PROCEDURE SP_GetAllLeveringen()
 BEGIN
     SELECT 
-         lvrn.bedrijfsnaam
-        ,cprs.contact_naam
-        ,lvng.eerstvolgende_levering
-    ;
+        lvrn.bedrijfsnaam,
+        cprs.contact_naam,
+        lvng.eerstvolgende_levering
+    FROM leveringen lvng
+    INNER JOIN leveranciers lvrn 
+        ON lvng.leverancier_id = lvrn.id
+    INNER JOIN contactpersonen cprs 
+        ON lvrn.contactpersoon_id = cprs.id;
 END ??
+
+DELIMITER ;
